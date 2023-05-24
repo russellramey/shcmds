48f53d7ad27c0bd28f5b8962d08b989db1e1f94f
Russell Ramey <doublercreative@gmail.com>
2023-05-24 09:41:28 -0500

    basic parser methods

---
+++ src/shcmds_parser.py
@@ -0,0 +1,22 @@
+import json
+import shcmds_sys as shcSYS
+
+# Read file as lines
+def readFile(file):
+    return
+
+# Search file for specific line
+def findLineFromFile(target, file, exact=False):
+    return
+
+# Parse line from test to object
+def formatLine(line: str=None):
+    return
+
+# Convert file text to json
+def textToJson(data: str=None):
+    return
+
+# Convert json to text
+def jsonToText(data: list=None):
+    return
