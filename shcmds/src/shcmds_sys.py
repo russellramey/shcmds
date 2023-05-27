977eb3d56b35bc88ca020e10353df3d97c50da05
Russell Ramey <doublercreative@gmail.com>
2023-05-27 13:48:30 -0500

    added comments sys utilities

--- shcmds/src/shcmds_sys.py
+++ shcmds/src/shcmds_sys.py
@@ -1,27 +1,58 @@
+#
+# Dependencies
+#
 import os
 
+#
+# Datastor path
+# Returns file path to datastore file for this module:
+# default: /usr/local/lib/ptyhonX.X/site-packages/shcmds/src/.shcdata
+#
 def getDatastorePath():
+    # Return path to data file
     return os.path.abspath(os.path.join(__file__, os.pardir)) + "/.shcdata"
-        
+
+#
+# Shell file path
+# Try to get the current shell profile filepath
+#
 def getShellProfilePath():
-    shell = getShellType()
     try:
+        # Get shell type
+        shell = getShellType()
+        # Default 
+        filepath = os.path.expanduser('~') + '/.profile'
+        # Zsh Shell
         if 'zsh' in shell:
-            file = os.path.expanduser( '~' ) + '/.zshrc'
+            filepath = os.path.expanduser('~') + '/.zshrc'
+        # Bash Shell
         if 'bash' in shell:
-            file = os.path.expanduser( '~' ) + '/.bashrc'
-        return file
+            filepath = os.path.expanduser('~') + '/.profile'
+        # Return path
+        return filepath
     except Exception as e:
-        print('Shell Profile error:')
+        print('Shell profile error:')
         print(e)
         return False
 
+#
+# Shell type
+# Returns current shell environment type
+#
 def getShellType():
     return os.environ['SHELL']
 
+#
+# Current Directory
+# Returns current working directory form shell
+#
 def getCurrentDir():
     return os.getcwd()
 
+#
+# File Exists
+# simply checks to see if a filepath exists
+#
 def fileExists(filepath):
     if(os.path.exists(filepath) is not False):
         return True
